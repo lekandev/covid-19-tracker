24f06b0d951741209463bf7a5efbd786c733a494
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-04-04 23:16:38 +0100

    🗺 create Map with react-leaflet

--- src/Map.js
+++ src/Map.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import "./Map.css";
+import { MapContainer as LeafletMap, TileLayer } from "react-leaflet";
 
-function Map() {
+function Map({ center, zoom }) {
     return (
-        <div className="map">
-            <h1>I am a Map</h1>
-        </div>
-    )
+      <div className="map" center={center} zoom={zoom}>
+        <LeafletMap>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+        </LeafletMap>
+      </div>
+    );
 }
 
 export default Map
