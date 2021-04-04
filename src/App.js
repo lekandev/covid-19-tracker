ed0708aee7ef680d4f5febf77c3b36cfbf456c6b
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-04-04 22:08:43 +0100

    🔮 prettify. minor updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import { MenuItem, FormControl, Select, Card, CardContent } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from "@material-ui/core";
 import InfoBox from "./InfoBox";
 import Map from "./Map";
 import "./App.css";
 import Table from "./Table";
 import { sortData } from "./util";
 import LineGraph from "./LineGraph";
+import "leaflet/dist/leaflet.css";
 
 function App() {
   // STATE = How to write a variable in REACT <<<<<<<
 
   const [countries, setCountries] = useState([]);
-  const [country, setCountry] = useState('worldwide');
+  const [country, setCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState({});
   const [tableData, setTableData] = useState([]);
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(3);
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/countries")
-    .then(response => response.json())
-    .then(data => {
-      setCountryInfo(data);
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
   }, []);
 
   useEffect(() => {
@@ -28,17 +37,17 @@ function App() {
 
     const getCountriesData = async () => {
       await fetch("https://disease.sh/v3/covid-19/countries")
-      .then((response) => response.json())
-      .then((data) => {
-        const countries = data.map((country) => ({
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
             name: country.country, // United States, United Kingdom
-            value: country.countryInfo.iso2 // UK, USA, FR
+            value: country.countryInfo.iso2, // UK, USA, FR
           }));
 
           const sortedData = sortData(data);
           setTableData(sortedData);
           setCountries(countries);
-      });
+        });
     };
 
     getCountriesData();
@@ -49,15 +58,18 @@ function App() {
 
     setCountry(countryCode);
 
-    const url = countryCode === 'worldwide' ? 'https://disease.sh/v3/covid-19/all' : `https://disease.sh/v3/covid-19/countries/${countryCode}`
+    const url =
+      countryCode === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
 
     await fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      setCountry(countryCode)
-      // All of the data from the country response
-      setCountryInfo(data);
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(countryCode);
+        // All of the data from the country response
+        setCountryInfo(data);
+      });
   };
 
   return (
@@ -81,12 +93,24 @@ function App() {
         </div>
 
         <div className="app__stats">
-          <InfoBox title="Coronavirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases} />
-          <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered} />
-          <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
+          <InfoBox
+            title="Coronavirus Cases"
+            cases={countryInfo.todayCases}
+            total={countryInfo.cases}
+          />
+          <InfoBox
+            title="Recovered"
+            cases={countryInfo.todayRecovered}
+            total={countryInfo.recovered}
+          />
+          <InfoBox
+            title="Deaths"
+            cases={countryInfo.todayDeaths}
+            total={countryInfo.deaths}
+          />
         </div>
 
-        <Map />
+        <Map center={mapCenter} zoom={mapZoom} />
       </div>
       <Card className="app__right">
         <CardContent>
